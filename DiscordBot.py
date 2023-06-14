5a148b8e0658cb1856a573e89b5efa1428cec47a
Maximilian Hans <69507108+UhhhItsMax@users.noreply.github.com>
2023-06-14 12:34:46 +0200

    Base

---
+++ DiscordBot.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+class MyDiscordBot(discord.Client):
+    async def on_ready(self):
+        print("Bot Ist An")
+
+# Lade die Umgebungsvariablen aus der .env-Datei
+load_dotenv()
+
+# Hole den Token aus den Umgebungsvariablen
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.all()
+discordBot = MyDiscordBot(intents=intents)
+discordBot.run(TOKEN)
